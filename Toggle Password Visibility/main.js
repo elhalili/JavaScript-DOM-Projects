1a733618d5a836e6718659114080e779ad2a8da7
elhalili <aminhalili2002@gmail.com>
2022-07-03 13:52:25 +0100

    add toggle password visibility

---
+++ Toggle Password Visibility/main.js
@@ -0,0 +1,13 @@
+const icon = document.getElementsByTagName('i')[0];
+
+icon.addEventListener('click', function () {
+    //select password input
+    const pswInput = this.parentElement.children[1];
+    const currentType = pswInput.getAttribute("type");
+    let currentClass = (currentType == "password"? "fa-eye-slash": "fa-eye");
+    let newClass = (currentClass == "fa-eye"? "fa-eye-slash": "fa-eye");
+
+    pswInput.setAttribute("type", (currentType == "text"? "password": "text"));
+    this.classList.remove(currentClass);
+    this.classList.add(newClass);
+})
